0910c3de9f26713c7a37ee01ef623802eeb5795e
Vitalii <60194255+VetalSh@users.noreply.github.com>
2021-02-06 01:05:17 +0200

    day2Add

--- js/main.js
+++ js/main.js
@@ -9,3 +9,15 @@ const flowersSlider = new Swiper('.flowers-slider', {
     prevEl: '.swiper-button-prev',
   },
 });
+
+const reviewsSlider = new Swiper('.reviews-slider', {
+  // Optional parameters
+  loop: true,
+  slidesPerView: 1,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
